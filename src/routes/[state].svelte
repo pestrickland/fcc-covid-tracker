a830e198762c5ed7f6abf474d5e11dd5477c7bab
Paul Strickland <p.e.strickland@gmail.com>
2020-10-06 22:39:19 +0100

    Add error handling for 404 and 500.

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,6 +1,22 @@
 <script context="module">
+  import stateNames from "../data/stateNames.js";
+
   export async function preload(page) {
-    return { state: page.params["state"] };
+    const state = page.params["state"];
+    if (stateNames.find((s) => s.abbreviation === state) === undefined) {
+      this.error(404, "State not found");
+      return;
+    }
+
+    try {
+      return { state: page.params["state"] };
+    } catch (e) {
+      this.error(
+        500,
+        "There was an error in calling the api. Please try again in 5 minutes."
+      );
+      return;
+    }
   }
 </script>
 
@@ -21,6 +37,5 @@
   </div>
 </div>
 
-
-<CovidStat></CovidStat>
-<CovidChart></CovidChart>
+<CovidStat />
+<CovidChart />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,16 @@
   import requests from "../data/requests.js";
   export async function preload() {
     try {
+      throw new Error("This is bad");
       const usStats = await requests.usStats();
       return { usStats };
-    } catch (e) {}
+    } catch (e) {
+      this.error(
+        500,
+        "There was an error in calling the api. Please try again in 5 minutes."
+      );
+      return;
+    }
   }
 </script>
 
